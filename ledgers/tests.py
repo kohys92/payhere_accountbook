fda43b112ff2be0399eae6dc76e436d22f3456a0
Young Soo Koh <kohys92@gmail.com>
2021-11-21 23:16:36 +0900

    ledger CRUD API - ADD: creating a ledger - FIX: temporary commit to fix model - ADD: total_income and total_expense in a ledger - ADD: Creating a ledger in two different cases - ADD: Get ledgers of the user - FIX: temporary commit to add soft deletion to model - ADD: is_removed needs to be fixed - ADD: delete a selected ledger - ADD: Edit a ledger: note, income, expense - ADD: Created a detail views and moved delete, edit methods into Detail view

--- ledgers/tests.py
+++ ledgers/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import json
+import jwt
 
-# Create your tests here.
+from django.test import TestCase, Client
+from ledgers.models import Ledger
+from users.models import User
+from config_settings import SECRET_KEY
+
+
+class TestLedgerView(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id = 1,
+            email = 'test@test.com',
+            password = 'Test123!',
+            last_name = 'Potter',
+            first_name = 'Harry',
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Ledger.objects.all().delete()
+
+    def test_post_ledger_success(self):
+        client = Client()
+
+        access_token = jwt.encode({'id' : 1}, SECRET_KEY, algorithm='HS256')
+        header = {'HTTP_Authorization' : access_token}
+
+        ledger = {
+            'note'     : 'This is test note',
+            'income'   : 50000,
+            'expense'  : 0,
+            'total'    : 0,
+            'category' : 'Food'
+        }
+        print('===============================')
+        print(ledger)
+        print('===============================')
+
+        response = client.post('/ledgers', json.dumps(ledger), **header, content_type = 'application/json')
+        self.assertEqual(response.status_code, 201)
--- ledgers/views.py
+++ ledgers/views.py
@@ -1,3 +1,148 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from users.models import User
+from ledgers.models import Ledger
+from users.userauth import user_auth
+
+
+class LedgerView(View):
+    # Create a ledger
+    @user_auth
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = request.user
+
+            ledger_list = Ledger.objects.filter(user = user.id)[::-1]
+
+            if Ledger.objects.filter(user = user.id).exists() and len(ledger_list) >= 1:
+                Ledger.objects.create(
+                    note          = data['note'],
+                    income        = data['income'],
+                    expense       = data['expense'],
+                    total_income  = ledger_list[0].total_income + data['income'],
+                    total_expense = ledger_list[0].total_expense + data['expense'],
+                    category      = data['category'],
+                    user          = User.objects.get(id=user.id)
+                )
+
+            else:
+                Ledger.objects.create(
+                    note          = data['note'],
+                    income        = data['income'],
+                    expense       = data['expense'],
+                    total_income  = data['income'],
+                    total_expense = data['expense'],
+                    category      = data['category'],
+                    user          = User.objects.get(id = user.id)
+                )
+
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+    # Read all the ledger
+    @user_auth
+    def get(self, request):
+        user = request.user
+
+        if not Ledger.objects.filter(user = user.id, is_removed = False).exists():
+            return JsonResponse({'message' : 'Ledger Does Not Exist'}, status = 404)
+
+        ledger_list = Ledger.objects.filter(user = user.id, is_removed = False)
+
+        result = []
+
+        for ledger in ledger_list:
+            result.append(
+                {
+                    'note'          : ledger.note,
+                    'income'        : ledger.income,
+                    'expense'       : ledger.expense,
+                    'total_income'  : ledger.total_income,
+                    'total_expense' : ledger.total_expense,
+                    'category'      : ledger.category
+                })
+
+        return JsonResponse({'message' : 'success', 'result' : result}, status = 200)
+
+
+class DetailView(View):
+    # Read a selected ledger with details
+    @user_auth
+    def get(self, request, post_id):
+        user = request.user
+
+        if not Ledger.objects.filter(id = post_id, user = user.id, is_removed = False).exists():
+            return JsonResponse({'message' : 'Ledger Does Not Exist'}, status = 404)
+
+        ledger = Ledger.objects.get(id = post_id, user = user.id, is_removed = False)
+
+        result = {
+                    'note'          : ledger.note,
+                    'income'        : ledger.income,
+                    'expense'       : ledger.expense,
+                    'total_income'  : ledger.total_income,
+                    'total_expense' : ledger.total_expense,
+                    'category'      : ledger.category
+                }
+
+        return JsonResponse({'message' : 'success', 'result' : result}, status = 200)
+
+# delete a selected ledger
+    @user_auth
+    def delete(self, request, post_id):
+        user = request.user
+
+        if not Ledger.objects.filter(id = post_id, user = user.id, is_removed = False).exists():
+            return JsonResponse({'message' : 'DOES NOT EXIST'}, status = 404)
+
+        selected_ledger               = Ledger.objects.get(id = post_id, user = user.id, is_removed = False)
+        selected_ledger.is_removed    = True
+        selected_ledger.save()
+
+        return JsonResponse({'message' : 'selected ledger removed'}, status = 200)
+
+    # Edit a selected ledger
+    @user_auth
+    def put(self, request, post_id):
+        try:
+            user = request.user
+
+            data = json.loads(request.body)
+
+            if not Ledger.objects.filter(id=post_id, user=user.id, is_removed = False).exists():
+                return JsonResponse({'message' : 'LEDGER DOES NOT EXIST'}, status = 404)
+
+            selected_ledger = Ledger.objects.get(id = post_id, user = user.id, is_removed = False)
+            previous_ledger = Ledger.objects.get(id = (post_id-1), user = user.id, is_removed = False)
+            next_ledgers = Ledger.objects.filter(user = user.id, is_removed = False)[post_id:]
+
+            selected_ledger.note          = data['note']
+            selected_ledger.income        = data['income']
+            selected_ledger.expense       = data['expense']
+            selected_ledger.total_income  = previous_ledger.total_income + data['income']
+            selected_ledger.total_expense = previous_ledger.total_expense + data['expense']
+            selected_ledger.save()
+
+            count = 0
+            for ledger in next_ledgers:
+                if count == 0:
+                    ledger.total_income  = selected_ledger.total_income + next_ledgers[count].income
+                    ledger.total_expense = selected_ledger.total_expense + next_ledgers[count].expense
+                    ledger.save()
+                    count += 1
+                else:
+                    ledger.total_income  = next_ledgers[count-1].total_income + next_ledgers[count].income
+                    ledger.total_expense = next_ledgers[count-1].total_expense + next_ledgers[count].expense
+                    ledger.save()
+                    count += 1
+
+            return JsonResponse({'message' : 'EDIT SUCCESS'}, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
