a0d38539d58f8c3052141a459aeff0b34e5b3616
Young Soo Koh <kohys92@gmail.com>
2021-11-22 22:37:01 +0900

    FIX: total_income and total_expense added to ledger model

--- ledgers/models.py
+++ ledgers/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
 from users.models import User
 
 
 class Ledger(models.Model):
-    date     = models.DateField(auto_now_add = True)
-    note     = models.CharField(max_length = 200)
-    income   = models.IntegerField(default = 0, null = True, blank = True)
-    expense  = models.IntegerField(default = 0, null = True, blank = True)
-    total    = models.IntegerField()
-    category = models.CharField(max_length = 45)
-    user     = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at    = models.DateField(auto_now_add = True)
+    note          = models.CharField(max_length = 200)
+    income        = models.IntegerField(default = 0, null = True, blank = True)
+    expense       = models.IntegerField(default = 0, null = True, blank = True)
+    total_income  = models.IntegerField(default = 0, null = True, blank = True)
+    total_expense = models.IntegerField(default = 0, null = True, blank = True)
+    category      = models.CharField(max_length = 45)
+    user          = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'ledgers'
