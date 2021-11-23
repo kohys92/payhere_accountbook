ae8b6c53cb068f13bc70c715dd01cfc066fa2a00
Young Soo Koh <kohys92@gmail.com>
2021-11-23 14:57:58 +0900

    - FIX: is_removed field modified to Boolean - FIX: IS_REMOVED variable removed

--- ledgers/models.py
+++ ledgers/models.py
@@ -3,11 +3,6 @@ from users.models import User
 
 
 class Ledger(models.Model):
-    IS_REMOVED = (
-        (0, 'exist'),
-        (1, 'removed'),
-    )
-
     created_at    = models.DateField(auto_now_add = True)
     note          = models.CharField(max_length = 200)
     income        = models.IntegerField(default = 0, null = True, blank = True)
@@ -15,7 +10,7 @@ class Ledger(models.Model):
     total_income  = models.IntegerField(default = 0, null = True, blank = True)
     total_expense = models.IntegerField(default = 0, null = True, blank = True)
     category      = models.CharField(max_length = 45)
-    is_removed    = models.IntegerField(default = 0, choices = IS_REMOVED, null = True, blank = True)
+    is_removed    = models.BooleanField(default = False, null = True, blank = True)
     user          = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
