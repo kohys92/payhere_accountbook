3cf6f47edfdc7744496f411aebfdb57000b46c78
Young Soo Koh <kohys92@gmail.com>
2021-11-23 14:42:45 +0900

    FIX: is_removed field option modified

--- ledgers/models.py
+++ ledgers/models.py
@@ -15,7 +15,7 @@ class Ledger(models.Model):
     total_income  = models.IntegerField(default = 0, null = True, blank = True)
     total_expense = models.IntegerField(default = 0, null = True, blank = True)
     category      = models.CharField(max_length = 45)
-    is_removed    = models.IntegerField(default = 0, choices = IS_REMOVED)
+    is_removed    = models.IntegerField(default = 0, choices = IS_REMOVED, null = True, blank = True)
     user          = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
