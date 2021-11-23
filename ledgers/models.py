aee0d785417d27a085aa5d797b98a9caf9b597c9
Young Soo Koh <kohys92@gmail.com>
2021-11-23 14:33:54 +0900

    ADD: Soft delete 기능 모델링

--- ledgers/models.py
+++ ledgers/models.py
@@ -3,6 +3,11 @@ from users.models import User
 
 
 class Ledger(models.Model):
+    IS_REMOVED = (
+        (0, 'exist'),
+        (1, 'removed'),
+    )
+
     created_at    = models.DateField(auto_now_add = True)
     note          = models.CharField(max_length = 200)
     income        = models.IntegerField(default = 0, null = True, blank = True)
@@ -10,6 +15,7 @@ class Ledger(models.Model):
     total_income  = models.IntegerField(default = 0, null = True, blank = True)
     total_expense = models.IntegerField(default = 0, null = True, blank = True)
     category      = models.CharField(max_length = 45)
+    is_removed    = models.IntegerField(default = 0, choices = IS_REMOVED)
     user          = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
